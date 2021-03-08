cf7dca2df4e7fe6081b6faf42fd40a38b9e7d0aa
Edivaldo Goncalves <edivaldo.goncalves@chuva.io>
2021-03-08 09:48:08 -0100

    Initial Commit

---
+++ TodoListSwift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodoListSwift
+//
+//  Created by Edivaldo Goncalves on 3/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodoListSwift/TodoListSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListSwiftApp.swift
+//  TodoListSwift
+//
+//  Created by Edivaldo Goncalves on 3/8/21.
+//
+
+import SwiftUI
+
+@main
+struct TodoListSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
